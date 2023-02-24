dd5637785a6a2f61366b2919fd78fe4d39b7ba25
Lachlan <lthom404@gmail.com>
2023-02-25 07:23:35 +1000

    Add project files.

---
+++ MrSmartRewriteV2/ConfigJSON.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrSmartRewriteV2
+{
+    internal struct ConfigJSON
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        public string Prefix { get; private set; }
+    }
+}
